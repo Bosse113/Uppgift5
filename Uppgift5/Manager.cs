a5ce1883c6cb6e7c9c2bea92d7a47b3ce8710a3b
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-06-09 09:55:04 +0200

    Add project files.

---
+++ Uppgift5/Manager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift5
+{
+    internal class Manager
+    {
+    }
+}
---
+++ Uppgift5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Uppgift5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Starta genom Manager
+        }
+    }
+}
