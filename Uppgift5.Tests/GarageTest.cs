d826c7c81a333a29f0bad801734e844efaf439e0
Bosse113 <49038474+Bosse113@users.noreply.github.com>
2026-06-15 16:00:53 +0200

    Added more tests

--- Uppgift5.Tests/GarageTest.cs
+++ Uppgift5.Tests/GarageTest.cs
@@ -5,12 +5,18 @@ namespace Uppgift5.Tests
 {
     public class GarageTest
     {
+//        
+//Om du behöver komma åt internal så får du lägga till i csproj filen i programmet du vill testa i ditt fall GarageProjektet
+//Dubbelklicka på projekt-filen och lägg till:
+//  < ItemGroup >
+//    < InternalsVisibleTo Include="Uppgift5.Tests"/>
+//  </ItemGroup>
+
         [Fact]
         
         public void Add_ValidVehicle_ReturnsTrue()
         {
             // Arrange
-            
             Garage<Vehicle> garage =
                 new Garage<Vehicle>(5);
 
@@ -29,6 +35,92 @@ namespace Uppgift5.Tests
         }
 
 
+        [Fact]
+        public void Add_GarageFull_ReturnsFalse()
+        {
+            // Arrange
+            var garage =
+                new Garage<Vehicle>(1);
+
+            garage.Add(
+                new Car(
+                    "ABC123",
+                    "Red",
+                    4,
+                    "Gasoline"));
+
+            // Act
+            bool result =
+                garage.Add(
+                    new Car(
+                        "DEF456",
+                        "Blue",
+                        4,
+                        "Diesel"));
+
+            // Assert
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void Find_ExistingVehicle_ReturnsVehicle()
+        {
+            // Arrange
+            var garage =
+                new Garage<Vehicle>(5);
+
+            garage.Add(
+                new Car(
+                    "ABC123",
+                    "Red",
+                    4,
+                    "Gasoline"));
+
+            // Act
+            Vehicle? result =
+                garage.Find("ABC123");
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+
+        [Fact]
+        public void Remove_ExistingVehicle_ReturnsTrue()
+        {
+            // Arrange
+            var garage =
+                new Garage<Vehicle>(5);
+
+            garage.Add(
+                new Car(
+                    "ABC123",
+                    "Red",
+                    4,
+                    "Gasoline"));
+
+            // Act
+            bool result =
+                garage.Remove("ABC123");
+
+            // Assert
+            Assert.True(result);
+        }
+        [Fact]
+        public void Find_NonExistingVehicle_ReturnsNull()
+        {
+            // Arrange
+            var garage =
+                new Garage<Vehicle>(5);
+
+            // Act
+            Vehicle? result =
+                garage.Find("XYZ999");
+
+            // Assert
+            Assert.Null(result);
+        }
     }
 }
 
